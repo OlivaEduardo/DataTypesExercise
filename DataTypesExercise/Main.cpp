dfd5d67998195c64011b2955babcd6d3d30400a5
OlivaEduardo <lokasocba@gmail.com>
2018-03-20 18:11:57 -0300

    Corrections

--- DataTypesExercise/Main.cpp
+++ DataTypesExercise/Main.cpp
@@ -7,15 +7,15 @@
 int main()
 {
 	//Data types sizes printings
-	printf_s("Size of Short Int: %d \n", sizeof(short int));
-	printf_s("Size of Int: %d \n", sizeof(int));
-	printf_s("Size of Float: %d \n", sizeof(float));
-	printf_s("Size of Double: %d \n", sizeof(double));
-	printf_s("Size of Long: %d \n", sizeof(long));
+	printf_s("Size of Short Int: %d \n", sizeof(short int));	 
+	printf_s("Size of Int: %d \n", sizeof(int));				
+	printf_s("Size of Float: %d \n", sizeof(float));			
+	printf_s("Size of Double: %d \n", sizeof(double));			
+	printf_s("Size of Long: %d \n", sizeof(long));				
 	printf_s("Size of Long Long: %d \n", sizeof(long long));
 	printf_s("Size of Size_t: %d \n", sizeof(size_t));
 
-	//Diferent numeric types printings for a same value with 2 diferent functions
+	//Different numeric types printings for a same value with 2 diferent functions
 	char sample = 65;
 
 	printf_s("\nValor decimal con printf_s: %d \n", sample);
@@ -25,25 +25,10 @@ int main()
 
 	std::cout << "\nValor decimal con cout: " << std::dec << static_cast<int>(sample) << " \n";
 	std::cout << "Valor hexadecimal con cout: " << std::hex << static_cast<int>(sample) << " \n";
-	std::cout << "Valor octal con cout: " << std::oct << static_cast<int>(sample) << " \n";
-	std::cout << "Valor caracter con cout: " << sample << " \n\n" << std::dec;
+	std::cout << "Valor octal con cout: " << std::oct << static_cast<int>(sample) << " \n" << std::dec;
+	std::cout << "Valor caracter con cout: " << sample << " \n\n";
 
 	//Numeric limits for data type printings
-	printf_s("Limite minimo de Short Int con printf: %d \n", std::numeric_limits<short int>::min());
-	printf_s("Limite maximo de Short Int con printf: %d \n", std::numeric_limits<short int>::max());
-	printf_s("Limite minimo de Int con printf: %d \n", std::numeric_limits<int>::min());
-	printf_s("Limite maximo de Int con printf: %d \n", std::numeric_limits<int>::max());
-	printf_s("Limite minimo de Float con printf: %f \n", std::numeric_limits<float>::min());
-	printf_s("Limite maximo de Float con printf: %f \n", std::numeric_limits<float>::max());
-	printf_s("Limite minimo de Double con printf: %f \n", std::numeric_limits<double>::min());
-	printf_s("Limite maximo de Double con printf: %f \n", std::numeric_limits<double>::max());
-	printf_s("Limite minimo de Long con printf: %d \n", std::numeric_limits<long>::min());
-	printf_s("Limite maximo de Long con printf: %d \n", std::numeric_limits<long>::max());
-	printf_s("Limite minimo de Long Long con printf: %d \n", std::numeric_limits<long long>::min());
-	printf_s("Limite maximo de Long Long con printf: %d \n", std::numeric_limits<long long>::max());
-	printf_s("Limite minimo de Size_t con printf: %d \n", std::numeric_limits<size_t>::min());
-	printf_s("Limite maximo de Size_t con printf: %d \n", std::numeric_limits<size_t>::max());
-
 	std::cout << "\nLimite minimo de Short Int (cout): " << std::numeric_limits<short int>::min() << " \n";
 	std::cout << "Limite maximo de Short Int (cout): " << std::numeric_limits<short int>::max() << " \n";
 	std::cout << "Limite minimo de Int (cout): " << std::numeric_limits<int>::min() << " \n";
@@ -63,16 +48,17 @@ int main()
 	int a = 0xFFFFFFFF;
 	unsigned int b = 0xFFFFFFFF;
 
-	std::cout << "\nValor de a: " << a << "\n"; //-1
-	std::cout << "Valor de b: " << b << "\n"; //Max positive value
+	std::cout << "\nValor de a: " << a << "\n"; //The hexa expression 0xFFFFFFFF represents the first negative decimal number because the signed data type divides it maximum size between positive and negative values.
+	std::cout << "Valor de b: " << b << "\n"; //This will represent the maximum positive value possible as the unsigned data type doesn't divide its size to represent negative values but just represent positive ones.
 
 	//Array
 	//Array declaration
-	int myArray[5];
+	const short int arraySize = 5;
+	int myArray[arraySize];
 
 	//Array loading
 	std::cout << "\nIngresar 5 elementos numericos enteros: \n";
-	for (short int i = 0; i < 5; i++)
+	for (short int i = 0; i < arraySize; i++)
 	{
 		std::cout << "Ingrese valor para la posicion " << i + 1 << "\n";
 		std::cin >> myArray[i];
@@ -81,7 +67,7 @@ int main()
 	}
 
 	//Array reading
-	for (short int i = 0; i < 5; i++)
+	for (short int i = 0; i < arraySize; i++)
 	{
 		std::cout << "El valor en la posicion " << i + 1 << " es: " << myArray[i] << "\n";
 	}
